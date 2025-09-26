93d04c0a5210f3bb0c011ec17c05c154f577bcea
norodondt <131435585+norodondt@users.noreply.github.com>
2025-09-26 21:22:57 +0200

    Update Sanity structure file to include a test comment and improve document type filtering

--- sanity/structure.ts
+++ sanity/structure.ts
@@ -1,10 +1,10 @@
 import {StructureBuilder} from 'sanity/structure'
-
+// test
 export const structure = (S: StructureBuilder) =>
   S.list()
     .title('Content')
     .items(
       S.documentTypeListItems().filter(
         (listItem) => !['ebookPage', 'chapter'].includes(listItem.getId()!),
-      ),
+      ), 
     )
