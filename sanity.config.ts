e8aad26697179828b28c63b5de5e77fc43abbf4d
norodondt <131435585+norodondt@users.noreply.github.com>
2025-09-26 21:27:40 +0200

    Refactor Sanity configuration to utilize environment variables for project ID and dataset

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,13 +10,14 @@ import {structureTool} from 'sanity/structure'
 import {markdownSchema} from 'sanity-plugin-markdown'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
+import {apiVersion, dataset, projectId} from './sanity/env'
 import {schemaTypes} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
+  projectId,
+  dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema: {
     types: schemaTypes,
