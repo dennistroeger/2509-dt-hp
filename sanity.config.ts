0ed6ab6661879e5d0faf76ffe8f0ea6adef7598a
norodondt <131435585+norodondt@users.noreply.github.com>
2025-09-26 21:27:00 +0200

    Update Sanity environment configuration to set default dataset and disable CDN usage

--- sanity.config.ts
+++ sanity.config.ts
@@ -10,14 +10,13 @@ import {structureTool} from 'sanity/structure'
 import {markdownSchema} from 'sanity-plugin-markdown'
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './sanity/env'
 import {schemaTypes} from './sanity/schemaTypes'
 import {structure} from './sanity/structure'
 
 export default defineConfig({
   basePath: '/studio',
-  projectId,
-  dataset,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema: {
     types: schemaTypes,
